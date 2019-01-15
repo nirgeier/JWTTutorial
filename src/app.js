4107896570a032c90f802d065580be007b605bb9
Nir Geier <nirgeier@gmail.com>
2019-01-15 23:03:17 +0200

    Step04-Validate token

--- src/app.js
+++ src/app.js
@@ -28,8 +28,22 @@ app.use('/login', (req, res, next) => {
             res.json(token);
         }
     );
-
 });
+
+// Add a secured route
+// We are calling the extractToken
+app.get('/secure', extractToken, (req, res, next) => {
+
+    // At this stage the token in on the request
+    // Verify that the token is valid
+    // verify a token symmetric
+    jwt.verify(req.token, SECRET, (err, data) => {
+        err ?
+            res.send("Failed to verify token") :
+            res.json(data);
+    });
+});
+
 // Listen on port 3000
 app.listen(3000, () => console.log('Server started on port 3000'));
 
@@ -39,4 +53,37 @@ const user = {
     firstName: "Nir",
     lastName: "Geier",
     email: "nirgeier@gmail.com"
+}
+
+/**
+ * Middleware for verifying the jwt token 
+ * The token is in the following format:
+ *    Authorization: Bearer <access_token>
+ **/
+function extractToken(req, res, next) {
+    // Get Authorization header which should be in the following format:
+    //
+    // Authorization: Bearer <access_token>
+    // 
+    const bearerHeader = req.headers['authorization'];
+
+    // Check if header was send in the request
+    if (typeof bearerHeader !== 'undefined') {
+        // Split the "Bearer <token>" value
+        const tokens = bearerHeader.split(' ');
+        // Save the token on the request 
+        req.token = tokens[1];
+        // Continue to the next Middleware
+        next();
+    } else {
+        // Set unauthorized status code 
+        //
+        // 403 Forbidden
+        // -------------
+        // The client does not have access rights to the content, 
+        // i.e. they are unauthorized, so server is rejecting to give 
+        // proper response. 
+        // Unlike 401, the client's identity is known to the server.
+        res.sendStatus(403);
+    }
 }
